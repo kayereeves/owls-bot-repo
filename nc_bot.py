9c94a1cdf042f8ab282db5113711c6cf8e21614e
kayereeves <kiannarachelmarie@gmail.com>
2022-03-11 15:52:34 -0800

    initial commit

---
+++ nc_bot.py
@@ -0,0 +1,375 @@
+import random
+import discord
+import os
+from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_option
+from dotenv import load_dotenv
+load_dotenv('.env')
+
+from nc_bot_sql import *
+
+tok = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+slash = SlashCommand(bot, sync_commands=True)
+
+images = [
+    'https://i.imgur.com/FD1h5Hi.gif',
+    'https://i.imgur.com/KzpDj6J.gif',
+    'https://i.imgur.com/ADRCu2K.gif',
+    'https://i.imgur.com/klmEBxc.gif']
+
+# commands from discord message
+
+# trade report
+# takes in trading data to create entry
+@slash.slash(name="tr",
+             description="Submit your trades to the database.",
+             options=[
+               create_option(
+                 name="sent",
+                 description="Input a list of the items/gbcs you sent with values.",
+                 option_type=3,
+                 required=True
+               ),
+               create_option(
+                 name="received",
+                 description="Input a list of items/gbcs you received with values.",
+                 option_type=3,
+                 required=True
+               ),
+               create_option(
+                 name="notes",
+                 description="Input any notes on the transactions - was it fair, unfair? Be concise!",
+                 option_type=3,
+                 required=False
+               ),
+               create_option(
+                 name="date",
+                 description="Input the date of the trade in YYYY-MM-DD format, e.g. 2021-08-13 (If ignored the default is today).",
+                 option_type=3,
+                 required=False
+               )
+             ],
+             guild_ids=[])
+                
+@bot.command()
+async def tr(ctx, sent: str, received: str, notes: str=None, date: str=None):
+    if date:
+        embed_date = f"`{date}`"
+    else:
+        embed_date = '`Today`'
+    if notes:
+        embed_notes = f"\n*** notes: {notes}"
+    else:
+        embed_notes = ''
+    send_message = discord.Embed (
+    title = 'Is this correct?',
+    description = f"{embed_date}\n```diff\n~ s: {sent}\n~ r: {received }{embed_notes}\n*** reported by: {ctx.author}```",
+    color = 0xffc000   
+    )               
+    send_message.set_thumbnail(url=random.choice(images))
+    send_message.set_footer(text="Make sure you have included values where possible!")
+
+    confirmation = await ctx.send(embed=send_message)
+    await confirmation.add_reaction('👍')
+    await confirmation.add_reaction('👎')
+    def check(reaction, user):
+            return user == ctx.author
+    reaction, user = await bot.wait_for("reaction_add", timeout=60.0, check=check)
+    confirmation_id = confirmation.id
+    if reaction.emoji == '👍' and reaction.message.id == confirmation_id:
+        result = add_trade(str(ctx.author), sent, received, date, notes)
+        if not result:
+            failed_message = discord.Embed (
+            title = 'Trade report failed!',
+            description = '```diff\n- Something went wrong, please try again! -```',
+            color = 0xa52d2f   
+            )                 
+            failed_message.set_thumbnail(url=random.choice(images))
+            failed_message.set_footer(text="Remember to format the date as YYYY-MM-DD and to avoid using single or double quotation characters!")
+            await confirmation.edit(embed=failed_message)
+            await confirmation.clear_reactions()
+        else:
+            user_stats = return_stats(ctx)
+            success_message = discord.Embed (
+            title = 'Trade reported successfully!',
+            description = '```xl\nYou have now reported ' + str(user_stats) + ' trades.```',
+            color = 0x789900   
+            )                 
+            success_message.set_thumbnail(url=random.choice(images))
+            await confirmation.edit(embed=success_message)
+            await confirmation.clear_reactions()
+    if reaction.emoji == '👎' and reaction.message.id == confirmation_id:
+        discard_message = discord.Embed (
+        title = 'Trade report discarded successfully!',
+        description = '```Alright, I have set that report on a course for the nearest blackhole. Please try again!```',
+        color = 0xa52d2f   
+        )                 
+        discard_message.set_thumbnail(url=random.choice(images))
+        await confirmation.edit(embed=discard_message)
+        await confirmation.clear_reactions()
+
+# trade history
+# returns the 20 most recent trade data of query
+@slash.slash(name="th",
+            description="View the 20 most recent reports for a particular item.",
+            options=[
+            create_option(
+                name="query",
+                description="The item you wish to view reports for.",
+                option_type=3,
+                required=True
+            )
+            ],
+            guild_ids=[])
+            
+@bot.command()
+async def th(ctx, query):
+    trade_results = return_trades(query)
+    if trade_results != False:
+        # trade_results[0] is the total results found, trade_results[1] is the embed title, trade_results[2] are the constructed pages
+        if trade_results[0] > 0:
+            i = 0
+            page = discord.Embed (
+                title = trade_results[1],
+                description = trade_results[2][0],
+                color = 0x789900
+    )
+        page.set_thumbnail(url=random.choice(images))
+        if trade_results[0] < 5:
+            page.set_footer(text="Submitting trade reports or searching the database is easy! Just type / to use the commands!")
+        else:
+            page.set_footer(text="Submitting trade reports or searching the database is easy! Just type / to use the commands!\nPage 1")
+        pages = [page]
+        try:
+            while i < 5:
+                i += 1
+                page = discord.Embed (
+                    title = trade_results[1],
+                    description = trade_results[2][i],
+                    color = 0x789900   
+                )               
+                page.set_thumbnail(url=random.choice(images))
+                page.set_footer(text="Submitting trade reports or searching the database is easy! Just type / to use the commands!\nPage " + str(i + 1))
+                pages.append(page) 
+        except IndexError:
+            print('womp')
+        max_i = i - 1 # max_i is the maximum index value available for trade_results[2][i]
+        message = await ctx.send(embed = pages[0])
+        if trade_results[0] > 4:
+            await message.add_reaction('⏮')
+            await message.add_reaction('◀')
+            await message.add_reaction('▶')
+            await message.add_reaction('⏭')
+            await message.add_reaction('⏹️')
+
+            def check(reaction, user):
+                return user == ctx.author
+
+            i = 0
+            reaction = None
+            message_id = message.id
+
+            while True:
+                if str(reaction) == '⏮' and reaction.message.id == message_id:
+                    i = 0
+                    await message.edit(embed = pages[i])
+                elif str(reaction) == '◀' and reaction.message.id == message_id:
+                    if i > 0:
+                        i -= 1
+                        await message.edit(embed = pages[i])
+                elif str(reaction) == '▶' and reaction.message.id == message_id:
+                    if i < max_i:
+                        i += 1
+                        await message.edit(embed = pages[i])
+                elif str(reaction) == '⏭' and reaction.message.id == message_id:
+                    i = max_i
+                    await message.edit(embed = pages[i])
+                elif str(reaction) == '⏹️' and reaction.message.id == message_id:
+                    await message.clear_reactions()
+                
+                try:
+                    reaction, user = await bot.wait_for('reaction_add', timeout = 30.0, check = check)
+                    await message.remove_reaction(reaction, user)
+                except:
+                    break
+            await message.clear_reactions()
+    else:
+        womp = discord.Embed (
+            description = '```diff\n-         Error Code: 34323948293423490         -\nAbsolutely no results could be found. Nada. Zero.\n-         Error Code: 34323948293423490         -```',
+            color = 0xa52d2f
+        )
+        womp.set_thumbnail(url=random.choice(images))
+        womp.set_footer(text="Submitting trade reports or searching the database is easy! Just type / to use the commands!")
+        message = await ctx.send(embed = womp)
+
+# returns the 40 most recent trade reports
+@slash.slash(name="logs",
+            description="View the 40 most recent trade reports.",
+            guild_ids=[])
+            
+@bot.command()
+async def logs(ctx):
+    trade_results = recent_trades(ctx)
+    if trade_results != False:
+        # trade_results[0] is the total results found, trade_results[1] is the embed title, trade_results[2] are the constructed pages
+        if trade_results[0] > 0:
+            i = 0
+            page = discord.Embed (
+                title = trade_results[1],
+                description = trade_results[2][0],
+                color = 0x789900
+    )
+        page.set_thumbnail(url=random.choice(images))
+        if trade_results[0] < 5:
+            page.set_footer(text="Submitting trade reports or searching the database is easy! Just type / to use the commands!")
+        else:
+            page.set_footer(text="Submitting trade reports or searching the database is easy! Just type / to use the commands!\nPage 1")
+        pages = [page]
+        try:
+            while i < 10:
+                i += 1
+                page = discord.Embed (
+                    title = trade_results[1],
+                    description = trade_results[2][i],
+                    color = 0x789900   
+                )               
+                page.set_thumbnail(url=random.choice(images))
+                page.set_footer(text="Submitting trade reports or searching the database is easy! Just type / to use the commands!\nPage " + str(i + 1))
+                pages.append(page) 
+        except IndexError:
+            print('womp')
+        max_i = i - 1 # max_i is the maximum index value available for trade_results[2][i]
+        message = await ctx.send(embed = pages[0])
+        if trade_results[0] > 4:
+            await message.add_reaction('⏮')
+            await message.add_reaction('◀')
+            await message.add_reaction('▶')
+            await message.add_reaction('⏭')
+            await message.add_reaction('⏹️')
+
+            def check(reaction, user):
+                return user == ctx.author
+
+            i = 0
+            reaction = None
+            message_id = message.id
+
+            while True:
+                if str(reaction) == '⏮' and reaction.message.id == message_id:
+                    i = 0
+                    await message.edit(embed = pages[i])
+                elif str(reaction) == '◀' and reaction.message.id == message_id:
+                    if i > 0:
+                        i -= 1
+                        await message.edit(embed = pages[i])
+                elif str(reaction) == '▶' and reaction.message.id == message_id:
+                    if i < max_i:
+                        i += 1
+                        await message.edit(embed = pages[i])
+                elif str(reaction) == '⏭' and reaction.message.id == message_id:
+                    i = max_i
+                    await message.edit(embed = pages[i])
+                elif str(reaction) == '⏹️' and reaction.message.id == message_id:
+                    await message.clear_reactions()
+                
+                try:
+                    reaction, user = await bot.wait_for('reaction_add', timeout = 30.0, check = check)
+                    await message.remove_reaction(reaction, user)
+                except:
+                    break
+            await message.clear_reactions()
+    else:
+        womp = discord.Embed (
+            description = '```diff\n-         Error Code: 34323948293423490         -\nAbsolutely no results could be found. Nada. Zero.\n-         Error Code: 34323948293423490         -```',
+            color = 0xa52d2f
+        )
+        womp.set_thumbnail(url=random.choice(images))
+        womp.set_footer(text="Submitting trade reports or searching the database is easy! Just type / to use the commands!")
+        message = await ctx.send(embed = womp)
+
+#returns amount of trades reported
+@slash.slash(name="stats",
+            description="View your stats.",
+            guild_ids=[])
+            
+@bot.command()
+async def stats(ctx):
+    user_results = return_stats(ctx)
+    stats = discord.Embed (
+        title = 'Stats for: ' + str(ctx.author),
+        description = '```xl\nTrades reported using Report-A-Trade Bot: ' + str(user_results) + '```',
+        color = 0x27a198
+    )
+    stats.set_thumbnail(url=random.choice(images))
+    stats.set_footer(text="Submitting trade reports or searching the database is easy! Just type / to use the commands!")
+                    
+    await ctx.send(embed=stats)
+
+# prints leaderboard of top members who submitted reports
+@slash.slash(name="leaderboard",
+            description="View the leaderboard.",
+            guild_ids=[])
+            
+@bot.command()
+async def leaderboard(ctx):
+    leaderboard = return_leaderboard(ctx)
+    leaderboard_embed = discord.Embed (
+        title = 'Report-A-Trade Bot Leaderboard',
+        description = leaderboard,
+        color = 0xa78900
+    )
+    leaderboard_embed.set_thumbnail(url=random.choice(images))
+    leaderboard_embed.set_footer(text="Submitting trade reports or searching the database is easy! Just type / to use the commands!")
+                    
+    await ctx.send(embed=leaderboard_embed)
+
+# prints help for use of bot
+@slash.slash(name="help",
+            description="View command info for Report-A-Trade Bot.",
+            guild_ids=[])
+            
+@bot.command()
+async def bothelp(ctx):
+    bothelp = discord.Embed (
+        title = 'Help',
+        description = 
+        """This bot helps record and display NC trade report data submitted by the community.
+        Type `/` followed by a command (or find the command in the slash commands menu) to interact with the bot.
+
+        \n\n
+        **/tr**\n
+        ```\n Submit a trade report. You will be prompted to input what you sent and what you received.\n\n
+        You may also choose to add a note to your report as well as record the date in YYYY-MM-DD format
+        (if you do not input a date, the current date will be used).\n
+
+        ```\n**/th**\n```
+        \nSearch the database. You will be prompted to input the item which you wish to view trade reports for.\n
+
+        ```\n**/logs**\n
+        ```\nView the 40 most recent reported trades.\n```\n
+        
+        **/stats**\n
+        ```\nView how many contributions you have made to the database.\n```\n
+
+        **/leaderboard**\n
+        ```\nView the top 10 reporters.\n```""",
+        color = 0xE5D8D9
+    )
+    bothelp.set_thumbnail(url=random.choice(images))
+                    
+    await ctx.send(embed=bothelp)
+
+# creates a download file of recently reported trading data
+@bot.command()
+async def download(ctx, date):
+    if (str(ctx.author) in ('imgonnageta#1995', 'maya#6899', 'rawxbee#8145')):
+        result = return_file(date)
+        file_path = "download.csv"
+        if result:
+            await ctx.send(file=discord.File(file_path))
+
+bot.run(tok)
---
+++ nc_bot_sql.py
@@ -0,0 +1,269 @@
+from datetime import datetime
+
+import mysql.connector
+from mysql.connector import Error
+
+import pandas as pd
+import csv
+from sqlalchemy import create_engine, types
+
+def runQuery(query, return_result=True):
+    conn = mysql.connector.connect()
+    if conn.is_connected():
+        cursor = conn.cursor()
+        cursor.execute(query)
+        if return_result:
+            try:
+                results = cursor.fetchall()
+                conn.close()
+                return results
+            except:
+                conn.close()
+                return none
+
+    conn.commit()
+    conn.close()
+    return None
+
+
+def update_user_logs(user):
+    query_retrieve = "SELECT * from trading_users where user_id = '%s'" %(user)
+    current_stats = runQuery(query_retrieve)
+
+    # insert new user
+    if len(current_stats) == 0:
+        # insert new users
+        query_update = "INSERT INTO trading_users(user_id, trades) VALUES ('%s', %s)"
+        args = (user, 1)
+
+    # update existing user
+    else:
+        query_update = "UPDATE trading_users SET trades = %s WHERE user_id = '%s'"
+        args = (current_stats[0][1]+1, user)
+
+    runQuery(query_update %args, False)
+
+
+def add_trade(user_id, sent, received, ds=None, notes=None):
+    query_update = "INSERT INTO transactions(loaded_at, transaction_id, user_id, traded, traded_for, ds, notes) VALUES (%s, '%s', '%s', '%s', '%s', %s, '%s')"
+
+    if ds:
+        try:
+            dt = datetime.strptime(ds, '%Y-%m-%d')
+            dt_fin = f"date('{dt}')"
+        except Exception as e:
+            print(e)
+            return False
+    else:
+        dt = datetime.now().strftime('%Y-%m-%d')
+        dt_fin = f"date('{dt}')"
+
+    if not (notes):
+        notes = ""
+
+    loaded_ds = 'now()'
+    transaction_id = user_id + datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    args = (loaded_ds, transaction_id, user_id, sent, received, dt_fin, notes)
+
+
+    try:
+        runQuery(query_update %args, False)
+        update_user_logs(user_id)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+def return_trades(query):
+    try:
+        item = query.lower()
+        query_retrieve = """SELECT * FROM
+                            (SELECT 
+                              traded AS item
+                              , traded_for AS returned
+                              , ds 
+                              , notes
+                              , user_id
+                              FROM transactions
+                            WHERE traded LIKE '%%%s%%'
+                            UNION ALL
+                            SELECT 
+                              traded AS item
+                              , traded_for AS returned
+                              , ds
+                              , notes
+                              , user_id
+                              FROM transactions
+                            WHERE traded_for LIKE '%%%s%%') trns
+                            GROUP BY 1,2,3,4,5
+                            ORDER BY ds DESC
+                            LIMIT 20;""" %(item, item)
+        results = runQuery(query_retrieve)
+        cleaned_page_results = ""
+        for row in results:
+            page_results = "" # page construction
+            cleaned_page_results = [] # pages separated into a list
+            total_found_results = 0 # total results found
+            total_page_results = 0 # total results for current page
+            add_result = "" # result to be added to current page
+            for row in results:
+                # Restrict how many total results are wanted, probably not needed because of the above limit but idk what doesn't kill me makes me stronger
+                if total_found_results < 20:
+                    if item in str(row[0]).lower():
+                        s = '! s: ' + row[0].replace('"','') + '\n'
+                    else:
+                        s = '~ s: ' + row[0].replace('"','') + '\n'
+                    if item in str(row[1]).lower():
+                        r = '! r: ' + row[1].replace('"','') + '\n'
+                    else:
+                        r = '~ r: ' + row[1].replace('"','') + '\n'
+                    if row[3]:
+                        n = '*** notes: ' + row[3].replace('"','') + '\n'
+                    else:
+                        n = ''
+                    formatted_date = row[2].strftime('%Y-%m-%d')
+                    # If total_page_results is 4, append that page to the list cleaned_page_results and begin a new page
+                    if total_page_results == 4:
+                        cleaned_page_results.append(page_results)
+                        total_page_results = 0
+                        page_results = ""
+                        d = '`' + formatted_date + '`\n'
+                        add_result = d + '```diff\n' + s + r + n + '\n```\n' # '*** reported by: ' + row[4].replace('"','') + '\n```\n'
+                        page_results += add_result
+                        total_found_results += 1
+                        total_page_results += 1
+                    # If total_page_results is less than 4 continue constructing current page
+                    else:
+                        if formatted_date in page_results:
+                            d = ''
+                        else:
+                            d = '`' + formatted_date + '`\n'
+                        add_result = d + '```diff\n' + s + r + n + '\n```\n' # '*** reported by: ' + row[4].replace('"','') + '\n```\n'
+                        page_results += add_result
+                        total_found_results += 1
+                        total_page_results += 1
+                else:
+                    break
+            # Append a final/only page that is not full
+            cleaned_page_results.append(page_results)
+            if total_found_results == 1:
+                embedTitle = str(total_found_results) + " recent trade result for '" + item + "':"
+            else:
+                embedTitle = str(total_found_results) + " recent trade results for '" + item + "':"
+            embed_details = [total_found_results, embedTitle, cleaned_page_results]
+        return embed_details
+    except Exception as e:
+        print(e)
+        return False
+
+def recent_trades(ctx):
+    try:
+        query_retrieve = """SELECT 
+                              traded AS item
+                              , traded_for AS returned
+                              , loaded_at 
+                              , notes
+                              , user_id
+                              FROM transactions
+                            ORDER BY loaded_at DESC
+                            LIMIT 40;"""
+        results = runQuery(query_retrieve)
+        page_results = "" # page construction
+        cleaned_page_results = [] # pages separated into a list
+        total_found_results = 0 # total results found
+        total_page_results = 0 # total results for current page
+        add_result = "" # result to be added to current page
+        for row in results:
+            # Restrict how many total results are wanted, probably not needed because of the above limit but idk what doesn't kill me makes me stronger
+            if total_found_results < 40:
+                s = '~ s: ' + row[0].replace('"','') + '\n'
+                r = '~ r: ' + row[1].replace('"','') + '\n'
+                if row[3]:
+                    n = '*** notes: ' + row[3].replace('"','') + '\n'
+                else:
+                    n = ''
+                    # If total_page_results is 4, append that page to the list cleaned_page_results and begin a new page
+                fdate = row[2].strftime('%Y-%m-%d')
+                if total_page_results == 4:
+                    cleaned_page_results.append(page_results)
+                    total_page_results = 0
+                    page_results = ""
+                    d = '`' + fdate + '`\n'
+                    add_result = d + '```diff\n' + s + r + n + '\n```\n' # + '*** reported by: ' + row[4].replace('"','') + '\n```\n'
+                    page_results += add_result
+                    total_found_results += 1
+                    total_page_results += 1
+                # If total_page_results is less than 4 continue constructing current page
+                else:
+                    if fdate in page_results:
+                        d = ''
+                    else:
+                        d = '`' + fdate + '`\n'
+                    add_result = d + '```diff\n' + s + r + n + '\n```\n' # + '*** reported by: ' + row[4].replace('"','') + '\n```\n'
+                    page_results += add_result
+                    total_found_results += 1
+                    total_page_results += 1
+            else:
+                break
+            embedTitle = str(total_found_results) + " most recent logged trades"
+            embed_details = [total_found_results, embedTitle, cleaned_page_results]
+        # Append a final/only page that is not full
+        cleaned_page_results.append(page_results)
+        # print(cleaned_results)
+        return embed_details
+    except Exception as e:
+        print(e)
+        return False
+
+def return_stats(ctx):
+    user = ctx.author
+    query_retrieve = "SELECT * from trading_users where user_id = '%s'" %(user)
+    results = runQuery(query_retrieve)[0]
+    str_results = '%d' %(results[1])
+    return str_results
+
+def return_leaderboard(ctx):
+    try:
+        query_retrieve = """SELECT 
+                              user_id
+                              , trades
+                              FROM trading_users
+                            ORDER BY trades DESC
+                            LIMIT 10;"""
+        results = runQuery(query_retrieve)
+        i = 0
+        emoji = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟']
+        leaderboard = ''
+        for row in results:
+            if i != 10:
+                user = row[0]
+                trades = row[1]
+                place = '```fix\n' + emoji[i] + ' ' + user + ' = ' + str(trades) + '```\n'
+                leaderboard += place
+                i += 1
+            else:
+                break
+        return leaderboard
+
+
+    
+    except Exception as e:
+        print(e)
+        return False
+
+def return_file(date):
+    mysql_user = 'doadmin'
+    mysql_password = 'yoozmkofqoda545k'
+    db_name = 'codenames'
+
+    connection_string = ''
+    engine = create_engine(f'mysql+mysqlconnector://{connection_string}')
+
+    file_path = "download.csv"
+    query = "select traded, traded_for, ds, notes from transactions where user_id like '%%#%%' and loaded_at >= '" + date + "' order by ds"
+    print(query)
+
+    df = pd.read_sql(sql=query, con=engine)
+    df.to_csv(file_path, index=False)
+    return True
